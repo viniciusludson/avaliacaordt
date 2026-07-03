ace08a0951e32861ee7c73d8656d97949c2c447b
medeirosclara-pixel <medeiros.clara@acad.ifma.edu.br>
2026-07-03 12:56:29 -0300

    Delete apk.operadordecaixa.py

--- apk.operadordecaixa.py
+++
@@ -1,5 +0,0 @@
-
-'''//MVP Web Fila Zero -versão inicial com React+Google Maps
-// Este código inicial foca na visualização do cliente e estrutura múltiplos perfis'''
-import {useEffect, useState} from "react";
-a
